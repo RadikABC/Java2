4b4b43bc16d0fd80d9206cccb91f861fa09c088a
Radik <radik.red.snow@yandex.ru>
2023-07-04 12:45:08 +0400

    addCodeBonus

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        int price = 1000; // стоимость билета
+        int mile = 20;   // стоимость одной мили
+        int bonus = price / mile;
+        System.out.println ("Вам начислено " + bonus + " миль");
 
     }
 }
